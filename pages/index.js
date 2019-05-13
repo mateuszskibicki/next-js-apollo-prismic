9a49fe9ecefeac497e826b66111d41c42e1b10c8
Mateusz Skibicki <skibickiengland@gmail.com>
2019-05-13 09:32:47 +0100

    getinitialprops works with apollo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import App from "../components/App";
 import PostList from "../components/PostList";
+import axios from "axios";
 
-export default () => (
-  <App>
-    <h1>app</h1>
-    <PostList />
-  </App>
-);
+const IndexPage = props => {
+  console.log(props);
+  return (
+    <App>
+      <h1>app</h1>
+      <PostList />
+    </App>
+  );
+};
+
+IndexPage.getInitialProps = async (req, res) => {
+  const data = await axios.get("https://api.github.com/users/mateuszskibicki");
+  return { test: data.data };
+};
+
+export default IndexPage;
